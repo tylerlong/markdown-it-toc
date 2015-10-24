c25b461fb091a0f105377277ee87fbb5f9e1687b
Tyler Long <tyler4long@gmail.com>
2015-10-24 21:39:18 +0800

    Compress code

--- fabfile.py
+++ fabfile.py
@@ -2,7 +2,9 @@ from fabric.api import local
 
 
 def compile():
-    local('browserify index.js -s markdownitTOC > dist/markdown-it-toc.min.js')
+    local('browserify index.js -s markdownitTOC > dist/markdown-it-toc.js')
+    local('uglifyjs --compress -- dist/markdown-it-toc.js > dist/markdown-it-toc.min.js')
+    local('rm -rf dist/markdown-it-toc.js')
 
 
 def copy():
